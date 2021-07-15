174312db326d9df625a8c4115425e3d411070833
charleslucky <1214176716qq>
2021-07-15 17:56:38 +0800

    update

--- LearnMapping.py
+++ LearnMapping.py
@@ -19,6 +19,25 @@ import PIL.Image as Image
 import random
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+def set_memory_growth():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+                logical_gpus = tf.config.experimental.list_logical_devices(
+                    'GPU')
+                logging.info(
+                    "Detect {} Physical GPUs, {} Logical GPUs.".format(
+                        len(gpus), len(logical_gpus)))
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            logging.info(e)
+    else:
+        logging.info("No GPU found!")
+set_memory_growth()
+# tf.config.experimental.list_physical_devices('GPU')
 print(tf.config.experimental.list_physical_devices('GPU'))
 
 
