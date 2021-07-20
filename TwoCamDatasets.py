4e3795951b6cf0a99f5d2044bedc3fb7e7de58bc
charleslucky <xingbod@gmail.com>
2021-07-20 14:59:25 +0800

    ubuntu

--- TwoCamDatasets.py
+++ TwoCamDatasets.py
@@ -8,13 +8,14 @@ from Utils import pack_raw
 class TwoCamDatasets(keras.utils.Sequence):
     """Helper to iterate over the data (as Numpy arrays)."""
 
-    def __init__(self, batch_size, img_size, input_img_size, input_img_paths, target_img_paths,packraw=True):
+    def __init__(self, batch_size, img_size, input_img_size, input_img_paths, target_img_paths,packraw=True,randomfliping=False):
         self.batch_size = batch_size
         self.img_size = img_size
         self.input_img_size = input_img_size
         self.input_img_paths = input_img_paths
         self.target_img_paths = target_img_paths
         self.packraw = packraw
+        self.randomfliping = randomfliping
 
     def __len__(self):
         return len(self.target_img_paths) // self.batch_size
@@ -32,6 +33,7 @@ class TwoCamDatasets(keras.utils.Sequence):
             x = np.zeros((self.batch_size,) + (256, 256) + (4,), dtype="float32")
         else:
             x = np.zeros((self.batch_size,) + (512, 512) + (1,), dtype="float32")
+        flip_flag = False
 
         for j, path in enumerate(batch_input_img_paths):
             img = io.imread(path)
@@ -47,6 +49,17 @@ class TwoCamDatasets(keras.utils.Sequence):
             img = img[xi:xi+wi,yi:yi+hi]
 #             img = (img / 127.5) - 1
             y[j] = np.expand_dims(img, 2)
+
+        if np.random.randint(2, size=1)[0] == 1:  # random flip
+            x = np.flip(x, axis=1)
+            y = np.flip(y, axis=1)
+        if np.random.randint(2, size=1)[0] == 1:
+            x = np.flip(x, axis=2)
+            y = np.flip(y, axis=2)
+        # print(x.shape)
+        # if np.random.randint(2, size=1)[0] == 1:  # random transpose
+        #     x = np.transpose(x, (0, 2, 1))
+        #     y = np.transpose(y, (0, 2, 1))
 
         return x, y
 
